0ddc219e5aee94a3932a35855b6175c3433fac29
markoshura <markoshura@yandex.ru>
2016-10-20 15:02:49 +0400

    everything OK

--- hash_table.cpp
+++ hash_table.cpp
@@ -1,30 +1,51 @@
+#include <cstdlib>
 #include <iostream>
-#include <cstring>
+#include <cmath>
+
+#include "doubleList.h"
+
+const int N_MAX = 10000;
+
 using namespace std;
-struct Node
-{
-    Node *next, *prev;
-    string *key;
-    string *value;
-};
 
-Node *hashTable[32];
-
-int my_hash(string &str)
-{
-    int hash_value = 0;
-    const char *p = str.c_str();
-    while(*p) {
-        hash_value = 31 * hash_value + *p;
-        ++p;
-    }
-    return abs(hash_value) % 32;
+int myHash(int x){
+    return (int)hash<int>()(x) % N_MAX; //Берётся хэш от x, явно приводится к int
 }
 
-Node *insert_node(string key, string value)
-{
-    int hash_value = 0;
-    Node *new_node;
-    hash_value = my_hash(key);
+struct hashTable{
+    List *values = new List[N_MAX];
+};
 
+void add( hashTable *table, int x)
+{
+    List *current = &(table->values[myHash(x)]);
+    add(current, x);
+}
+
+int find(hashTable *table, int check_x){
+
+    List *current = &(table->values[myHash(check_x)]);
+    find(current, check_x);
+}
+
+void pop(hashTable *table, int pop_x){
+
+    List * current = &(table->values[myHash(pop_x)]);
+    int index = find(current, pop_x);
+    pop(current, index);
+}
+
+int main(void){
+
+    hashTable *table = new hashTable;
+    for (int i = 0; i < 20; i++) {
+        int x = rand() / 1000000;
+        add(table, x);
+    }
+    cout << find(table, 1804) << endl;
+    add(table, 2);
+    cout << find(table, 2) << endl;
+    pop(table, 2);
+    cout << find(table, 2) << endl;
+    return 0;
 }
